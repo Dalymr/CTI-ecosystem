d785eb2dc713506738c1aceabbf5a810d9cad1d4
daly marwen <dalymarouan@gmail.com>
2024-05-09 10:16:05 +0100

    added a script for enabling proxy on containers

---
+++ enable-proxy.sh
@@ -0,0 +1,6 @@
+export http_proxy="http://172.22.200.200:8080/"
+export https_proxy="http://172.22.200.200:8080/"
+export HTTP_PROXY="http://172.22.200.200:8080/"
+export HTTPS_PROXY="http://172.22.200.200:8080/"
+export no_proxy="localhost,::1,172.0.0.1/8,127.0.0.1/8,10.0.0.0/8"
+export NO_PROXY="localhost,::1,172.0.0.1/8,127.0.0.1/8,10.0.0.0/8"
